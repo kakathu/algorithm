55e3e207c6e64d394babe31e0c99c1457eda124e
jian.wu <wujiankaka@126.com>
2019-07-09 17:19:35 +0800

    update DepthFirstSearch

--- src/dfs/DepthFirstSearch.java
+++ src/dfs/DepthFirstSearch.java
@@ -3,21 +3,29 @@ package dfs;
 import java.util.Stack;
 
 public class DepthFirstSearch {
-    private static int N = 5;
+    private static int N = 6;
+    //    private static int[][] map = {
+//            {0, 1, 1, 0, 0},
+//            {0, 0, 1, 0, 1},
+//            {0, 0, 1, 0, 0},
+//            {1, 1, 0, 0, 1},
+//            {0, 0, 1, 0, 0}
+//    };
     private static int[][] map = {
-            {0, 1, 1, 0, 0},
-            {0, 0, 1, 0, 1},
-            {0, 0, 1, 0, 0},
-            {1, 1, 0, 0, 1},
-            {0, 0, 1, 0, 0}
+            {0, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 1, 1},
+            {0, 0, 1, 0, 0, 0},
+            {1, 1, 0, 0, 1, 0},
+            {0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0}
     };
-    private static boolean[] visited = new boolean[N + 1];
+    private static boolean[] visited = new boolean[N];
 
     public static void recursiveDFS(int start) {
         visited[start] = true;
-        System.out.println(start + " ");
-        for (int i = 1; i <= N; i++) {
-            if (map[start - 1][i - 1] == 1 && !visited[i]) {
+        System.out.println((start + 1) + " ");
+        for (int i = 0; i < N; i++) {
+            if (map[start][i] == 1 && !visited[i]) {
                 recursiveDFS(i);
             }
         }
@@ -28,15 +36,21 @@ public class DepthFirstSearch {
         visited[start] = true;
         Stack<Integer> s = new Stack<Integer>();
         s.push(start);
-        System.out.print(start + " ");
+        System.out.print((start + 1) + " ");
         while (!s.empty()) {
-            int v = s.pop();
-            for (int i = 1; i <= N; i++) {
-                if (!visited[i] && map[v - 1][i - 1] == 1) {
+            int v = s.peek();
+            boolean flag = false;
+            for (int i = 0; i < N; i++) {
+                if (!visited[i] && map[v][i] == 1) {
                     visited[i] = true;
                     s.push(i);
-                    System.out.print(i + " ");
+                    System.out.print((i + 1) + " ");
+                    flag = true;
+                    break;
                 }
+            }
+            if (!flag) {
+                s.pop();
             }
         }
     }
@@ -45,10 +59,10 @@ public class DepthFirstSearch {
 //        System.out.println("dfs");
 //        System.out.println(map[0][0]);
 //        System.out.println(visited[0]);
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             if (!visited[i]) {
-                //recursiveDFS(i);
-                nonRecursiveDFS(i);
+                recursiveDFS(i);
+//                nonRecursiveDFS(i);
             }
         }
     }
